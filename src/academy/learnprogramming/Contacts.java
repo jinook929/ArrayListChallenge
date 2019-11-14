f6391b3dab9c932f574155619971eb200cae10cc
jinook929 <jinook929@gmail.com>
2019-11-13 22:05:18 -0500

    add variables

--- src/academy/learnprogramming/Contacts.java
+++ src/academy/learnprogramming/Contacts.java
@@ -1,4 +1,11 @@
 package academy.learnprogramming;
 
 public class Contacts {
+    private String name;
+    private String phoneNumber;
+
+    public Contacts(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/academy/learnprogramming/MobilePhone.java
+++ src/academy/learnprogramming/MobilePhone.java
@@ -1,4 +1,8 @@
 package academy.learnprogramming;
 
+import java.util.ArrayList;
+
 public class MobilePhone {
+    ArrayList<String> name = new ArrayList<>();
+    ArrayList<String> phoneNumber = new ArrayList<>();
 }
